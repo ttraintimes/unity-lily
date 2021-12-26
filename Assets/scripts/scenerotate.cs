9f2842961d2479de87fea0aab2c807d9b10158f5
ttraintimes <ttraintimes@gmail.com>
2021-12-26 11:15:04 +0000

    3

---
+++ Assets/scripts/scenerotate.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Attach this script to a GameObject to rotate around the target position.
+public class scenerotate : MonoBehaviour
+{
+    //Assign a GameObject in the Inspector to rotate around
+    public GameObject target;
+
+    void Update()
+    {
+        // Spin the object around the target at 1 degree/second.
+        transform.RotateAround(target.transform.position, Vector3.up, Time.deltaTime);
+    }
+}
