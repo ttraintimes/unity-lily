25f39387b1e1f9cf0f1361eb065dedb1a89bae74
ttraintimes <ttraintimes@gmail.com>
2022-01-08 00:44:45 +0000

    player

---
+++ Assets/SceneChange.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour
+{
+    // Start is called before the first frame update
+   Ray ray;
+     RaycastHit hit;
+ 
+     void Update()
+     {
+         ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+         if (Physics.Raycast(ray, out hit) && Input.GetMouseButton(0))
+         {
+             if (hit.collider.name == "planet1") 
+             {
+                 SceneManager.LoadScene("BODYBOX");
+             }
+         }
+     }
+ }
