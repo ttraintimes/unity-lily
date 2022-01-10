6fcdfb34ee7d8ef341fc6a5291e4449b6d0c5254
ttraintimes <ttraintimes@gmail.com>
2022-01-10 18:52:56 +0000

    info

---
+++ Assets/scripts/SpriteSwitch.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteSwitch : MonoBehaviour
+{
+    // Start is called before the first frame update
+public SpriteRenderer spriteRenderer;
+public Sprite newSprite, circle;
+
+
+   
+    void Start()
+    {
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = circle;
+    }
+   
+void Update () {
+		if (Input.GetMouseButtonDown(0)) {
+			spriteRenderer.sprite = newSprite; 
+		}
+	}
+}
