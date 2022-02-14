7e7e2836f0f9c6f9e94b0e7164eefd4f098918a9
ttraintimes <ttraintimes@gmail.com>
2022-02-14 20:03:33 +0000

    menu scene

---
+++ Assets/MenuUI.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuUI : MonoBehaviour
+{
+    public void enterBtn()
+    {
+        SceneManager.LoadScene("start");
+    }
+
+    public void quitBtn()
+    {
+        Application.Quit();
+    }
+
+}
