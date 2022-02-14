9bf28b77c079cf66325d01aa49c708fd633247ef
ttraintimes <ttraintimes@gmail.com>
2022-02-14 20:52:52 +0000

    setting menu update

--- Assets/scripts/waitforsec.cs
+++ Assets/scripts/waitforsec.cs
@@ -13,13 +13,13 @@ public class waitforsec : MonoBehaviour
  
      private IEnumerator ActivationRoutine()
      {        
-         //Wait for 14 secs.
+         //Wait for 3 secs.
          yield return new WaitForSeconds(3);
  
          //Turn My game object that is set to false(off) to True(on).
          objectToActivate.SetActive(true);
  
-         //Turn the Game Oject back off after 1 sec.
+         //Turn the Game Oject back off after 10 sec.
          yield return new WaitForSeconds(10);
  
          //Game object will turn off
