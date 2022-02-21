e22bd67d5149404408db4577218adb3867c80f66
ttraintimes <ttraintimes@gmail.com>
2022-02-21 05:55:05 +0000

    speedup

--- Assets/scripts/player/PlayerContrl.cs
+++ Assets/scripts/player/PlayerContrl.cs
@@ -4,57 +4,52 @@ using System.Collections;
 
 //Script for moving a gameObject smoothly
 //Usage: Attach the character controller component to the gameobject that you want to move
-//Souce: https://stackoverflow.com/questions/62391105/how-to-add-jump-in-c-sharp-script-in-unity3d-using-character-controller
-namespace UnityLibary
-{
-    public class PlayerContrl : MonoBehaviour
-    {
+//Souce: https: //stackoverflow.com/questions/62391105/how-to-add-jump-in-c-sharp-script-in-unity3d-using-character-controller
+
+namespace UnityLibary {
+    public class PlayerContrl : MonoBehaviour {
         // place the gameobject that you want to move to the controller placeholder
         public CharacterController controller;
 
-        public float speed = 5f;
-    //    public float jumpSpeed = 2.0f;
- public float gravity = 5.0f;
- private Vector3 movingDirection = Vector3.zero;
+        public float speed=7f;
+        // public float jumpSpeed = 2.0f;
+        public float gravity=5.0f;
+        private Vector3 movingDirection=Vector3.zero;
 
-        void Update()
-        {
-         //   if (controller.isGrounded && Input.GetButton("Jump")) {
-             if (controller.isGrounded) {
-        // movingDirection.y = jumpSpeed;
-         movingDirection.y = 1.0f;
-     }
-     movingDirection.y -= gravity * Time.deltaTime;
-     controller.Move(movingDirection * Time.deltaTime);
+        void Update() {
+            if (Input.GetKey(KeyCode.LeftShift)) {
+                speed=14f;
+            }
+            else {
+                speed=7f;
+            }
 
+            if (controller.isGrounded) {
+                // movingDirection.y = jumpSpeed;
+                movingDirection.y=1.0f;
+            }
 
-           // float x = Input.GetAxis("Horizontal");
-            float z = Input.GetAxis("Vertical");
+            movingDirection.y -=gravity * Time.deltaTime;
+            controller.Move(movingDirection * Time.deltaTime);
 
-           // Vector3 move = transform.right * x + transform.forward * z;
-            Vector3 move = transform.forward * z;
+            float z=Input.GetAxis("Vertical");
+            Vector3 move=transform.forward * z;
 
             controller.Move(move * speed * Time.deltaTime);
 
-            //Rotate clockwise
-            if (Input.GetKey(KeyCode.D))
-            {
+            if (Input.GetKey(KeyCode.D)) {
                 transform.RotateAround(transform.position, Vector3.up, 20 * Time.deltaTime);
             }
-            // Rotate anticlockwise
-            if (Input.GetKey(KeyCode.A))
-            {
+            if (Input.GetKey(KeyCode.A)) {
                 transform.RotateAround(transform.position, -Vector3.up, 20 * Time.deltaTime);
             }
-             if (Input.GetKey(KeyCode.Q))
-             {
-                 transform.Rotate(Vector3.left, 3.0f * Time.deltaTime);
-             }
-             if (Input.GetKey(KeyCode.Z))
-             {
-                 transform.Rotate(Vector3.right, 3.0f * Time.deltaTime);
-             }
-      
+            if (Input.GetKey(KeyCode.Q)) {
+                transform.Rotate(Vector3.left, 3.0f * Time.deltaTime);
+            }
+            if (Input.GetKey(KeyCode.Z)) {
+                transform.Rotate(Vector3.right, 3.0f * Time.deltaTime);
+            }
+
 
         }
 
