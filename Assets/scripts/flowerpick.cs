551f5cba80280564c80b93ba3efba426f259611a
ttraintimes <ttraintimes@gmail.com>
2022-04-28 17:07:07 +0100

    pick flower

---
+++ Assets/scripts/flowerpick.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class flowerpick : MonoBehaviour
+{
+    public Text flowernum;
+    [HideInInspector]
+    public  int n=0;
+    [HideInInspector]
+    
+    public void flower()
+    {
+        n++;
+        flowernum.text = n+" picked";
+    }
+}
---
+++ Assets/scripts/pick.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pick : MonoBehaviour
+{
+    void OnMouseDown()
+    {
+      FindObjectOfType<flowerpick>().flower();
+      gameObject.SetActive(false);
+
+    }
+}
