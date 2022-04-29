2161b557f7e126309fc9d549409c5a2565a90cef
ttraintimes <ttraintimes@gmail.com>
2022-04-29 03:21:17 +0100

    hit balls

---
+++ Assets/scripts/playerflower.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class playerflower : MonoBehaviour
+{
+    GameObject raycastedObj;
+    public Text flowernum;
+
+    [SerializeField] private int InteractionRange = 1;
+    [SerializeField] private LayerMask flowerLayer;
+
+    // Update is called once per frame
+
+
+    void Start()
+    {
+        flowernum.enabled = false;
+    }
+
+    void Update()
+    {
+        RaycastHit hit;
+        Vector3 fwd = transform.TransformDirection(Vector3.forward);
+
+        if (Physics.Raycast(transform.position, fwd, out hit, InteractionRange, flowerLayer.value))
+        {
+            if (hit.collider.CompareTag("flower"))
+            {
+                flowernum.enabled = true;
+            //    raycastedObj = hit.collider.gameObject;
+            }
+        }
+        else
+        {
+            flowernum.enabled = false;
+        }
+
+    }
+}
