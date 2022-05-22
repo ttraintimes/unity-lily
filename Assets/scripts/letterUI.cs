1aa9bb237639e493eedf5b053217c35e0f0d6a00
ttraintimes <ttraintimes@gmail.com>
2022-05-22 06:06:58 +0100

    1

---
+++ Assets/scripts/letterUI.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class letterUI : MonoBehaviour
+{
+    GameObject raycastedObj;
+    ObjectInteraction referencedScript;
+    public GameObject letterScene;
+    public UnityEvent onMenuAppear;
+
+    [SerializeField] private int InteractionRange = 2;
+    [SerializeField] private LayerMask scenetransitionLayer;
+
+    // Update is called once per frame
+
+
+    void Start()
+    {
+        letterScene.SetActive(false);
+    }
+
+    void Update()
+    {
+        RaycastHit hit;
+        Vector3 fwd = transform.TransformDirection(Vector3.forward);
+
+        if (Physics.Raycast(transform.position, fwd, out hit, InteractionRange, scenetransitionLayer.value))
+        {
+            if (hit.collider.CompareTag("letter"))
+            {
+                letterScene.SetActive(true);
+                onMenuAppear.Invoke();
+                raycastedObj = hit.collider.gameObject;
+                referencedScript = raycastedObj.GetComponent<ObjectInteraction>();
+                Time.timeScale = 0;
+            }
+        }
+        else
+        {
+            letterScene.SetActive(false);
+        }
+
+    }
+}
